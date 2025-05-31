cb65539de9958305de0f7efa7348fd2eaf1457ad
SalahAmm <salahamm4year@gmail.com>
2025-05-31 15:11:10 +0100

    Exercise 2

--- pointer.c
+++ pointer.c
@@ -3,24 +3,21 @@
 
 int main () {
 
-    float a , b;
-    float *P1;
-    float *P2;
+    int N;
+    int *P = &N;
+    
+    printf("Enter a Number: ");
+    scanf("%d" , P);
 
-    P1 = &a;
-    P2 = &b;
+    if (*P % 2 == 0 ) {
+        printf("The number is EVEN");
+    }
+    else {
+        printf("the number is ODD");
+    }
+    
 
-    printf("Enter the Number A: ");
-    scanf("%f" , &a);
-    printf("Enter the Number B: ");
-    scanf("%f" , &b);
 
-    printf("the sum is %.2f \n" , *P1 + *P2);
-    printf("the difference is %.2f\n" , *P1 - *P2);
-    printf("the product is %.2f\n" , *P1 * *P2);
-    printf("the divistion is %.2f" , *P1 / *P2);
-
-     
 
     return 0;
 }
