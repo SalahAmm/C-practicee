6d2f772d9650e277ea0f851c9ecedb2192698a74
SalahAmm <salahamm4year@gmail.com>
2025-05-31 16:07:39 +0100

    Pointer with array

--- pointer.c
+++ pointer.c
@@ -1,37 +1,28 @@
 #include <stdio.h>
 
-void change (int *A , int *B) {
-    int temp , C ;
-    if (*A >=0 && *B >= 0) {
-        temp = *A;
-        *A = *B;
-        *B = temp;
-    } else if (*A < 0 || *B < 0) {
-                
-        C = *A;
-        *A= *A + *B;
-        *B= C * *B;
-    }
-
-
-
-}
+       
 
 int main () {
-    int A , B ,C , temp; 
-    int *P1 = &A;
-    int *P2 = &B;
 
-    printf("Enter The Number A: ");
-    scanf("%d" ,P1);
-    printf("Enter the Number B: ");
-    scanf("%d", P2);
+    int T[100];
+    int N;
+    int *P;
 
-    change(P1 ,P2);
+    P = T;
+    printf("Enter the size of the array: ");
+    scanf("%d" , &N);
 
-    printf("The Value of A is %d and the Value of B is %d" , *P1 , *P2);
+    printf("Enter the element of the array: \n");
 
+    for (P = T ; P < N + T ; P++) {
+        printf("T[%d] = " , P - T);
+        scanf("%d" , P);
 
+    }
+    for (P = T ; P < N + T ; P++) {
+        printf("%d", *P);
+
+    }
 
     return 0;
 }
