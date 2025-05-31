ef59598cc32f37b710285ec4f77e00ff51ee033d
SalahAmm <salahamm4year@gmail.com>
2025-05-31 16:51:00 +0100

    removing element in array with pointer

--- pointer.c
+++ pointer.c
@@ -5,24 +5,40 @@
 int main () {
 
     int T[100];
-    int N;
-    int *P;
+    int N , X;
+    int *P1 , *P2;
+    P1 = T ;
 
-    P = T;
     printf("Enter the size of the array: ");
     scanf("%d" , &N);
 
-    printf("Enter the element of the array: \n");
-
-    for (P = T ; P < N + T ; P++) {
-        printf("T[%d] = " , P - T);
-        scanf("%d" , P);
-
+    printf("Enter the element in the array: \n");
+    for ( P1 = T ; P1 < N + T ; P1++) {
+        printf("T[%d]: ", P1 - T);
+        scanf("%d" , P1);
     }
-    for (P = T ; P < N + T ; P++) {
-        printf("%d", *P);
 
+    printf("Array without removing X: \n");
+    for ( P1 = T ; P1 < N + T ; P1++) {
+        printf("%d \n", *P1);
     }
+
+    printf("Enter the Number X: \n"); 
+    scanf("%d" , &X);
+
+    printf("Array without X: \n");
+    P2 = T;
+    for (P1 = T ; P2 < T + N ; P1++) {
+        *P2 = *P1;
+        if (*P2 != X) {
+            P2++;
+        }
+    }
+    N = P2 - T ;
+    for (P1 = T ; P1 < T + N ; P1++) {
+        printf("%d \n" , *P1);
+    }
+
 
     return 0;
 }
