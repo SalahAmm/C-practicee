244f9173d50899bbbb872f3169a18bc8405d2cda
SalahAmm <salahamm4year@gmail.com>
2025-05-31 15:39:53 +0100

    Exercise 3 using function

--- pointer.c
+++ pointer.c
@@ -1,5 +1,21 @@
 #include <stdio.h>
 
+void change (int *A , int *B) {
+    int temp , C ;
+    if (*A >=0 && *B >= 0) {
+        temp = *A;
+        *A = *B;
+        *B = temp;
+    } else if (*A < 0 || *B < 0) {
+                
+        C = *A;
+        *A= *A + *B;
+        *B= C * *B;
+    }
+
+
+
+}
 
 int main () {
     int A , B ,C , temp; 
@@ -11,16 +27,7 @@ int main () {
     printf("Enter the Number B: ");
     scanf("%d", P2);
 
-    if (*P1 >=0 && *P2 >= 0) {
-        temp = *P1 ;
-        *P1 = *P2;
-        *P2 = temp;
-    } else if (*P1 < 0 || *P2 < 0) {
-                
-        C = *P1;
-        *P1 = *P1 + *P2;
-        *P2 = C * *P2;
-    }
+    change(P1 ,P2);
 
     printf("The Value of A is %d and the Value of B is %d" , *P1 , *P2);
 
