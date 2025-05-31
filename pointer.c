ab73176ebb3ba6d84caa370c0fdc60b123b21332
SalahAmm <salahamm4year@gmail.com>
2025-05-31 15:32:29 +0100

    Exercise 3

--- pointer.c
+++ pointer.c
@@ -2,18 +2,27 @@
 
 
 int main () {
+    int A , B ,C , temp; 
+    int *P1 = &A;
+    int *P2 = &B;
 
+    printf("Enter The Number A: ");
+    scanf("%d" ,P1);
+    printf("Enter the Number B: ");
+    scanf("%d", P2);
 
-    int N;
-    int *P = &N;
-    
-    printf("Enter a Number: ");
-    scanf("%d" , P);
-    for (int i = 1 ; i <= 18 ; i++ ) {
-        if (*P % i == 0) {
-            printf("%d\n", i);
-        }
+    if (*P1 >=0 && *P2 >= 0) {
+        temp = *P1 ;
+        *P1 = *P2;
+        *P2 = temp;
+    } else if (*P1 < 0 || *P2 < 0) {
+                
+        C = *P1;
+        *P1 = *P1 + *P2;
+        *P2 = C * *P2;
     }
+
+    printf("The Value of A is %d and the Value of B is %d" , *P1 , *P2);
 
 
 
