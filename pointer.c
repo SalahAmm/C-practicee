3812b8f0b05bd37518e3eb0fa607228ac0050e41
SalahAmm <salahamm4year@gmail.com>
2025-06-01 17:14:37 +0100

    Reverse an Array

--- pointer.c
+++ pointer.c
@@ -7,6 +7,7 @@ int main () {
     int T[100];
     int N , X;
     int *P1 , *P2;
+    int temp;
     P1 = T ;
 
     printf("Enter the size of the array: ");
@@ -18,25 +19,20 @@ int main () {
         scanf("%d" , P1);
     }
 
-    printf("Array without removing X: \n");
+    printf("Original Array: \n");
     for ( P1 = T ; P1 < N + T ; P1++) {
-        printf("%d \n", *P1);
+        printf("|%d|", *P1);
     }
 
-    printf("Enter the Number X: \n"); 
-    scanf("%d" , &X);
-
-    printf("Array without X: \n");
-    P2 = T;
-    for (P1 = T ; P2 < T + N ; P1++) {
-        *P2 = *P1;
-        if (*P2 != X) {
-            P2++;
-        }
+    for (P1 = T , P2 = T+N-1 ; P1 < P2 ; P1++ , P2--){
+        temp = *P1;
+        *P1 = *P2;
+        *P2 = temp;
     }
-    N = P2 - T ;
+
+    printf("The Reversed Array: ");
     for (P1 = T ; P1 < T + N ; P1++) {
-        printf("%d \n" , *P1);
+        printf("|%d|" , *P1);
     }
 
 
