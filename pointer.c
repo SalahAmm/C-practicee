dc2244299956dafb65c412a481367d69e7be4bb3
SalahAmm <salahamm4year@gmail.com>
2025-06-02 11:14:14 +0100

    basic memory allocation in c

--- pointer.c
+++ pointer.c
@@ -1,42 +1,20 @@
 #include <stdio.h>
+#include <stdlib.h>
 
        
 
 int main () {
 
-    int A[100] , B[100];
-    int N ;
-    int *PA , *PB;
-    int temp;
-    PA = A ;
-
-    printf("Enter the size of the array A : ");
-    scanf("%d" , &N);
-
-    printf("Enter the element in the array: \n");
-    for ( PA = A ; PA < N + A ; PA++) {
-        printf("A[%d]: ", PA - A);
-        scanf("%d" , PA);
-
-    }
-
-
+    int *P1 ,*P2 , *P3;
+    P1 = malloc(100);
+    printf("Allocate 100 bits in %p\n" , P1);
+    P2 = malloc(50);
+    printf("Allocate 100 bits in %p\n" , P2);
+    free(P1);
+    printf("free 100 bits in %p\n" , P1);
+    P3 = malloc(40);
+    printf("Allocate 100 bits in %p\n" , P3);
     
-    for ( PA = A ; PA < N + A-1 ; PA++) {
-        for (PB =PA+1 ; PB < A + N ; PB++) {
-            if ( *PA > *PB ) {
-                temp = *PA;
-                *PA = *PB;
-                *PB = temp;
-            }
-        }
-    } 
-
-    printf("Array sorted from small to large:  ");
-    for (PA = A ; PA < A + N ; PA++) {
-        printf("|%d|" , *PA);
-    }
-
 
     return 0;
 }
